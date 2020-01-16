7ccdc06db7d9dd2e2a0519250eb0f126f87ca2b7
minfanzhang <zhang.minfan@mail.utoronto.ca>
2020-01-15 20:07:45 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,20 @@ const svg = canvas.append('svg')
     .attr('width', 600)
     .attr('height', 600);
 
-svg.append('rect');
-svg.append('circle');
-svg.append('line');
+svg.append('rect')
+    .attr('x', 100)
+    .attr('y', 100)
+    .attr('width', 150)
+    .attr('height', 100)
+    .attr('fill', 'blue');
+svg.append('circle')
+    .attr('cx', 350)
+    .attr('cy', 150)
+    .attr('r', 50)
+    .attr('fill', 'red');
+svg.append('line')
+    .attr('x1', 450)
+    .attr('y1', 100)
+    .attr('x2', 550)
+    .attr('y2', 250)
+    .attr('stroke', 'grey');
