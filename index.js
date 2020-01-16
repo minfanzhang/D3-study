f6be915da9c8f2b7b1d31a4dfe7322b292dac116
minfanzhang <zhang.minfan@mail.utoronto.ca>
2020-01-15 21:24:15 -0500

    Update index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const data = [
+    {width: 200, height: 300, fill: 'red'}
+];
+
+const svg = d3.select('svg');
+
+const rect = svg.select('rect')
+    .data(data)
+    .attr('width', function(d, i, n){ 
+        console.log(i)
+        console.log(n)
+        return d.width 
+    })
+    .attr('height', function(d){ return d.height })
+    .attr('fill', function(d){ return d.fill });
