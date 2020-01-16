e343248a17e50029e323c0883f851cb5777ca830
minfanzhang <zhang.minfan@mail.utoronto.ca>
2020-01-15 20:02:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const canvas = d3.select(".canvas");
 
-const svg = canvas.append('svg');
+const svg = canvas.append('svg')
+    .attr('width', 600)
+    .attr('height', 600);
 
 svg.append('rect');
 svg.append('circle');
