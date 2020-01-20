0206e4bed155abd6336d6bfb20bf657c3add9f23
minfanzhang <zhang.minfan@mail.utoronto.ca>
2020-01-19 21:57:29 -0500

    None

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const data = [
-    {width: 200, height: 300, fill: 'red'}
+    {width: 200, height: 300, fill: 'red'},
+    {width: 100, height: 150, fill: 'green'},
+    {width: 50, height: 75, fill: 'yellow'}
 ];
 
 const svg = d3.select('svg');
 
-const rect = svg.select('rect')
+const rect = svg.selectAll('rect')
     .data(data)
     .attr('width', (d, i, n) => { return d.width })
     .attr('height', (d) => { return d.height })
     .attr('fill', (d) => { return d.fill });
+
+console.log(rect);
